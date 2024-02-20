51493375d5fb510ba5e675ca97516dce6b4f2fa2
PenkovEugene <penkovzheny@gmail.com>
2024-02-20 22:09:56 +0500

    измененны пути к изображениям в HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import bomb from './img/bomb.png';
+import arrow from './img/arrowPlay.png'
 
 import './homePage.css'
 
@@ -6,12 +8,12 @@ function HomePage () {
   return (
     <main className='homePageContainer'>
       <div className="logoContainer">
-        <img src="bomb.png" alt="Mine Logo" />
+        <img src={bomb} alt="Mine Logo" />
       </div>
       <div className="buttonContainer">
         <button>
           <Link to="/settings">Play</Link>
-          <img src="arrowPlay.png" alt="arrow play"/>
+          <img src={arrow} alt="arrow play"/>
         </button>
         <button className='ratingButton'>
           <Link to="/rating">Rating</Link>
